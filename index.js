5a39ae4c0af349aac97d89b3a54976073ae93565
Sher Shah <sher.shah010@gmail.com>
2018-12-30 14:35:34 -0800

    removed bad/buggy code

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ show: false, icon:'../icon_2.ico' });
-
+  win = new BrowserWindow({ show: false, icon:'../icon_2.ico', skipTaskbar: false });
   // and load the index.html of the app.
   win.loadFile('index.html');
   win.maximize();
